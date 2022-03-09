bdb4b697e42a31c9aa90d5e2a4cfaa0cccf8cf98
NeftaliLimon <nef.nl66@gmail.com>
2022-03-08 21:54:55 -0600

    first commit

---
+++ src/uaslp/objetos/parcial2/Empleado.java
@@ -0,0 +1,17 @@
+package uaslp.objetos.parcial2;
+
+public class Empleado extends Persona {
+    private Double sueldoBruto;
+
+    public void calcularSalarioNeto(){
+
+    }
+
+    public Double getSueldoBruto() {
+        return sueldoBruto;
+    }
+
+    public void setSueldoBruto(Double sueldoBruto) {
+        this.sueldoBruto = sueldoBruto;
+    }
+}
